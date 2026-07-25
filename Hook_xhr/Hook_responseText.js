6981a1e9f3eb4a3a2352a700d96f4e35de4c87cb
0xsdeo <i0xsdeo@foxmail.com>
2026-07-25 11:13:52 +0800

    Update Hook_responseText.js

--- Hook_xhr/Hook_responseText.js
+++ Hook_xhr/Hook_responseText.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Hook_responseText
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/0xsdeo/Hook_JS
 // @version      2025-01-05
 // @description  try to take over the world!
-// @author       You
+// @author       0xsdeo
 // @match        http://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
@@ -17,7 +17,7 @@
 
     Object.defineProperty(XMLHttpRequest.prototype, "responseText", {
         get: function () {
-            let response_text = get_accessor.call(this,XMLHttpRequest.prototype);
+            let response_text = get_accessor.call(this);
             console.log(response_text);
 
             return response_text; // 当网站js获取目标属性值时调用原属性getter返回结果
